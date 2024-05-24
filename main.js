afad3c4f82cd120062ac7b8398f61e1b7578c9fc
paczek654321 <paczek654321@gmail.com>
2024-05-24 02:07:27 +0200

    finnished the themes system

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const colors =
         "primary-color": "rgb(129, 107, 129)",
         "secondary-color": "rgb(2, 0, 34)",
         "project-text-color": "rgb(3, 0, 27)",
-        "distinct-link-color": "rgb(9, 0, 78)"
+        "distinct-link-color": "rgb(9, 0, 78)",
+        "projects-border-color": "black",
     },
     "high_contrast":
     {
-        "primary-color": "black",
-        "secondary-color": "black",
+        "primary-color": "#101010",
+        "secondary-color": "#101010",
         "project-text-color": "cyan",
-        "distinct-link-color": "yellow" 
+        "distinct-link-color": "yellow",
+        "projects-border-color": "white",
     }
 }
 
@@ -24,6 +26,7 @@ function setColors(preset)
     root.style.setProperty("--secondary-color", colors[preset]["secondary-color"])
     root.style.setProperty("--project-text-color", colors[preset]["project-text-color"])
     root.style.setProperty("--distinct-link-color", colors[preset]["distinct-link-color"])
+    root.style.setProperty("--projects-border-color", colors[preset]["projects-border-color"])
 }
 
 window.onload = ()=>
